3abe34caf3c8ac4b66853ffb3720f51735f6cbfe
Thomas Sherer <olc0710@yahoo.com>
2019-10-31 21:15:31 -0700

    Pushed the lab progress as of the end of class time.

--- js/app.js
+++ js/app.js
@@ -42,18 +42,67 @@ if (collegeDegree === 'no' || collegeDegree === 'n') {
 var workedInLA = prompt('Did I work in Los Angeles, CA?').toLowerCase();
 console.log('work in Los Angeles?:', workedInLA);
 
-if (workedInLA === 'yes' || workedInLA === 'y') {
-  alert('No, it was the San Fernando valley, north of LA!');
-} else {
+if (workedInLA === 'no' || workedInLA === 'n') {
   alert('Good attention to detail!');
+} else {
+  alert('No, it was the San Fernando valley, north of LA!');
 }
 
 var hireMe = prompt('From what you read, would you hire me?');
+console.log('Hire me?: ', hireMe);
+
 if (hireMe === 'yes' || hireMe === 'y') {
   alert('Glad to hear that! Keep me in your network!');
 } else {
   alert('Really? Missing a prize....');
 }
+
+var answerSix = parseInt(prompt('What number am I thinking of?'));
+console.log('first guess?: ', answerSix);
+
+for( var i = 0; i < 6; i++) {
+  console.log('What number?:', typeof answerSix);
+
+  if (answerSix === 5) {
+    alert('You got it!');
+    console.log('Correct guess: ', answerSix);
+    i = 6;
+  } else if (answerSix < 5) {
+    alert('No, too low. Try again.');
+    answerSix = parseInt(prompt('Guess again: '));
+    console.log('New guess?: ', answerSix);
+  } else if (answerSix > 5) {
+    alert('No, too high. Try again.');
+    answerSix = parseInt(prompt('Guess again: '));
+    console.log('New guess?: ', answerSix);
+  }
+  console.log('Correct guess confirmed: ', answerSix);
+}
+
+/*As a user, I would like to be guided to an answer through a series of feedback responses so that I 
+can learn more about the site owner.
+Add a 6th question to the guessing game that takes in a numeric input by prompting the user to guess 
+a number.
+Indicates through an alert if the guess is “too high” or “too low”.
+It should give the user exactly four opportunities to get the correct answer.
+After all attempts have been exhausted, tell the user the correct answer. Consider using a loop of 
+some sort. */
+
+/*As a user, I would like to guess the answer to a question that could have many possibilities so 
+that I can have fun with with a guessing game.
+Add a 7th question that has multiple possible correct answers that are stored in an array.
+Give the user 6 attempts to guess the correct answer.
+The guesses will end once the user guesses a correct answer or they run out of attempts.
+Display all the possible correct answers to the user.
+Consider using a loop of some sort for this question. */
+
+/*As a user, I would like to know my final score so that I can know how well I did.
+Keep track of the total number of correct answers. At the end tell them how many they got correct 
+out of the 7 questions asked.*/
+
+/*In addition to the user stories stated above, continue to use console.logs throughout the code to 
+track the questions that are being asked to the user, and add additional CSS to style your HTML page. */
+
 
 // Once the ordeal is finished, I bid the user farewell as follows;
 alert(
