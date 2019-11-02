c6c5c34671e88de63ca7cbfecaccbd693e9de23a
Thomas Sherer <olc0710@yahoo.com>
2019-11-01 22:31:21 -0700

    Added the seventh question and correct answer tally to js file, per requirements.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 'use strict';
 
+var tallyWins = 0 // Initialize the count of correct answers
+
 //Ask user for their name
 var userName = prompt('Before we begin, may I have your name, please?');
-console.log(userName);
+console.log('User entered name ', userName);
 alert(
   'Welcome, ' +
     userName +
@@ -14,9 +16,11 @@ var birthState = prompt('First question: Was I born in Kentucky?').toLowerCase()
 console.log('Born in Kentucky?:', birthState);
 if (birthState === 'no' || birthState === 'n') {
   alert('Nailed It! Must have been the accent that gave it away.');
+  tallyWins++;
 } else {
   alert('Nope card.');
 }
+console.log('Correct guesses: ', tallyWins);
 
 // Second Question
 var gradCity = prompt('Did I graduate in San Jose, CA?').toLowerCase();
@@ -24,9 +28,11 @@ console.log('Graduate in San Jose?:', gradCity);
 
 if (gradCity === 'yes' || gradCity === 'y') {
   alert('Yes, I did. Good job!');
+  tallyWins++;
 } else {
   alert('OK, so you slept through that part.');
 }
+console.log('Correct guesses: ', tallyWins);
 
 // Third Question
 var collegeDegree = prompt(
@@ -35,70 +41,106 @@ var collegeDegree = prompt(
 console.log('College Degree', collegeDegree);
 if (collegeDegree === 'no' || collegeDegree === 'n') {
   alert('Ding! Ding! Ding! You are correct!  I thought I might catch you with that one.');
+  tallyWins++;
 } else {
   alert('Sorry. It was High School + a 2-year degree later.');
 }
+console.log('Correct guesses: ', tallyWins);
 
+//Fourth Question
 var workedInLA = prompt('Did I work in Los Angeles, CA?').toLowerCase();
 console.log('work in Los Angeles?:', workedInLA);
 
 if (workedInLA === 'no' || workedInLA === 'n') {
   alert('Good attention to detail!');
+  tallyWins++;
 } else {
   alert('No, it was the San Fernando valley, north of LA!');
 }
+console.log('Correct guesses: ', tallyWins);
 
+//Fifth Question
 var hireMe = prompt('From what you read, would you hire me?');
 console.log('Hire me?: ', hireMe);
 
 if (hireMe === 'yes' || hireMe === 'y') {
   alert('Glad to hear that! Keep me in your network!');
+  tallyWins++;
 } else {
   alert('Really? Missing a prize....');
 }
+console.log('Correct guesses: ', tallyWins);
 
+//Sixth Question
 var grandkidQty = parseInt(prompt('I have grandkids, but fewer than 10. Care to guess how many?'));
-console.log('first guess?: ', grandkidQty);
+console.log('first guess?: ', grandkidQty); //Display pre-loop guess in console
 
 for( var i = 0; i < 4; i++) {
-  console.log('What number?:', typeof grandkidQty);
+  console.log('What number?:', typeof grandkidQty); // Confirm current guess entering the loop
 
-  if (grandkidQty === 9) {
+  if (grandkidQty === 9) { // Catching the correct guess...
     alert('You got it!');
     console.log('Correct guess: ', grandkidQty);
+    tallyWins++;
     i = 5;
-  } else if (grandkidQty < 9) {
+  } else if (grandkidQty < 9) { // Guess was too few...
     alert('No, too few. Try again.');
     grandkidQty = parseInt(prompt('Guess again: '));
     console.log('New guess?: ', grandkidQty);
-  } else if (grandkidQty > 9) {
+  } else if (grandkidQty > 9) { //Guess was too many...
     alert('No, too many (or is it? Well, not that many YET). Try again.');
-    grandkidQty = parseInt(prompt('Guess again: '));
+    grandkidQty = parseInt(prompt('Guess again: ')); //New guess to re-enter the loop.
     console.log('New guess?: ', grandkidQty);
   }
-  console.log('Correct guess confirmed: ', grandkidQty);
 }
-alert('For those who did not guess correctly, I have 9 - so far - and they are a joy!');
+alert('For those who did not guess correctly, I have 9 - so far - and they are a joy! Spend much time around me and you will hear of them.');
+console.log('Correct guesses: ', tallyWins);
 
+//Seventh Question
+var favoriteCookies;
+favoriteCookies = ['ginger snaps', 'snickerdoodles', 'oatmeal raisin', 'molasses']; //Some of the top correct answers
+var i = 0;
+var cookieGuess = prompt('Can you guess one of my favorite cookie types? - Food, not web...');
 
-/*As a user, I would like to guess the answer to a question that could have many possibilities so 
-that I can have fun with with a guessing game.
-Add a 7th question that has multiple possible correct answers that are stored in an array.
-Give the user 6 attempts to guess the correct answer.
-The guesses will end once the user guesses a correct answer or they run out of attempts.
-Display all the possible correct answers to the user.
-Consider using a loop of some sort for this question. */
-
-/*As a user, I would like to know my final score so that I can know how well I did.
-Keep track of the total number of correct answers. At the end tell them how many they got correct 
-out of the 7 questions asked.*/
+while(i < 6) { // Let looping ensue! Six rounds only.
+  console.log('What type of cookie?', cookieGuess);
+  if (cookieGuess = favoriteCookies[0]) { // If 'ginger snaps' was guessed...
+    alert('You got it! Ginger snaps is one of them.');
+    console.log('Correct guess: ', cookieGuess);
+    tallyWins++;
+    break;
+  } else if (cookieGuess = favoriteCookies[1]) { // If 'snickerdoodles' was guessed...
+    alert('You got it! Snickerdoodle is one of them.');
+    console.log('Correct guess: ', cookieGuess);
+    tallyWins++;
+    break;
+  } else if (cookieGuess = favoriteCookies[2]) { // If 'oatmeal raisin' was guessed...
+    alert('You got it! Oatmeal raisin is probably my favorite of all.');
+    console.log('Correct guess: ', cookieGuess);
+    tallyWins++;
+    break;
+  } else if (cookieGuess = favoriteCookies[3]) { // If 'molasses' was guessed...
+    alert('You got it! Molasses cookies are among them.');
+    console.log('Correct guess: ', cookieGuess);
+    tallyWins++;
+    break;
+  } else if (i < 5) {
+    cookieGuess = prompt('Sorry, that was not on my list. Try again...');
+    i++;
+  } else if (i = 5) {
+    alert('Ohhhh. NO SOUP FOR YOU (and no COOKIES, either)!');
+}
+alert('Correct answers would have been...');
+document.write( favoriteCookies.toString());
+console.log('Correct guesses: ', tallyWins);
 
 /*In addition to the user stories stated above, continue to use console.logs throughout the code to 
 track the questions that are being asked to the user, and add additional CSS to style your HTML page. */
 
-
-// Once the ordeal is finished, I bid the user farewell as follows;
+// Once the ordeal is finished, I bid the user farewell as follows, including their number of correct guesses;
 alert(
   'See, ' +
     userName +
-    ', that was not so bad! As to that final answer, we should get some lunch and chat a bit... Hmm?');
+    ', that was not so bad! You got a total of ' + tallyWins + ' right out of the 7 questions.  As to that "hire me" answer, we should get some lunch and chat a bit... Hmm?');
+
+// End of lab.
